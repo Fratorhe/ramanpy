180494f41ac5a9ddf7fd95a464cf431387773978
Fran <fratorhe@vki.ac.be>
2020-09-24 15:55:50 +0200

    updates flake8

--- ramanpy/tests/test_examples.py
+++ ramanpy/tests/test_examples.py
@@ -1,11 +1,16 @@
-def test_one_plus_one_is_two():
-    "Check that one and one are indeed two."
-    assert 1 + 1 == 2
+
 
 import numpy as np
+
 from ..refraction import snell
+
+
 # (The above is equivalent to `from example.refraction import snell`.
 # Read on for why.)
+
+def test_one_plus_one_is_two():
+    """Check that one and one are indeed two."""
+    assert 1 + 1 == 2
 
 
 def test_perpendicular():
@@ -23,6 +28,6 @@ def test_perpendicular():
 
 def test_air_water():
     n_air, n_water = 1.00, 1.33
-    actual = snell(np.pi/4, n_air, n_water)
+    actual = snell(np.pi / 4, n_air, n_water)
     expected = 0.5605584137424605
     assert np.allclose(actual, expected)
