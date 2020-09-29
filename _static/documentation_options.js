1c8f8b33a0bec7ca20572c36d3fb7b7146d6a57a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-09-29 12:56:13 +0000

    Update docs after building Travis build 8 of Fratorhe/ramanpy
    The docs were built from the branch 'master' against the commit
    b5a1cef8fbb3bc8dd3a31c0beee9e950ab0f39ff.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/Fratorhe/ramanpy/jobs/731272656.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post14+g487e38b',
+    VERSION: '0.post15+gb5a1cef',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
